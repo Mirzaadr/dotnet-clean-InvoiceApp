8ee00117ced6d83738bd681aeda96b8148bd1cfe
Mirzaadr <mirzaadr34@yahoo.com>
2025-06-11 07:21:11 +0700

    set clutureinfo globally

--- InvoiceApp.Web/DependencyInjection.cs
+++ InvoiceApp.Web/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using InvoiceApp.Web.Services;
 
 namespace InvoiceApp.Web;
@@ -6,7 +7,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddControllersWithViews();
+        // define en-US as culture info, so the app will use dot for decimal
+        var cultureInfo = new CultureInfo("en-US");
+        cultureInfo.NumberFormat.CurrencySymbol = "IDR "; // use IDR for currency instead of $
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+        services.AddControllersWithViews().AddDataAnnotationsLocalization();
         services.AddScoped<IInvoiceFormViewModelFactory, InvoiceFormViewModelFactory>();
         return services;
     }
